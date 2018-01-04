ce40247ca1b1e6b3f6611cc26d45db5d9904b45d
Jacob Whitish <jacobwhitish@gmail.com>
2018-01-03 20:42:20 -0800

    customized launch request

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var Alexa = require("alexa-sdk");
 
 var handlers = {
   "HelloIntent": function () {
-    this.response.speak("Hello, Codecademy");
+    this.response.speak("Hello, Jacob. Welcome to Codecademy");
     this.emit(':responseReady');
   },
   "LaunchRequest": function () {
-    this.response.speak("Welcome to Codecademy");
+    this.response.speak("Jacob, Welcome to Codecademy. Congratulations on your new alexa skill.");
     this.emit(':responseReady');
   }
 };
